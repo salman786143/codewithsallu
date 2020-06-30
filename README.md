5b8cb7bc1f7480c06079a78264e6e11414ea2096
salman <salmanpatel453@gmail.com>
2020-07-01 02:34:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# codewithsallu
---
+++ src/main/java/com/example/microservice/MicroServicesApplication.java
@@ -0,0 +1,14 @@
+package com.example.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicesApplication.class, args);
+		System.out.println("salman");
+	}
+
+}
