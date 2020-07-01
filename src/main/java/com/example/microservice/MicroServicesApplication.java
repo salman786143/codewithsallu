ab4e843ee324fddde0d81a35528c889b7d7303f7
salman <salmanpatel453@gmail.com>
2020-07-01 10:54:42 +0530

    first commit

--- src/main/java/com/example/microservice/MicroServicesApplication.java
+++ src/main/java/com/example/microservice/MicroServicesApplication.java
@@ -9,6 +9,7 @@ public class MicroServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MicroServicesApplication.class, args);
 		System.out.println("salman");
+		
 	}
 	
 
